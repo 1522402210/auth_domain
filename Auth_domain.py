6831d9d2efcdb33c893484605610fdabd8167b9a
ihoney <102505481@qq.com>
2018-02-23 00:54:09 +0800

    代码优化，去除清理池功能

--- Auth_domain.py
+++ Auth_domain.py
@@ -1,86 +1,73 @@
+# -*- coding: utf8 -*-
 import requests
 import logging
-import threading
 from concurrent import futures
-import datetime
 
-max_threads = 50  # 最大并发线程数
 logging.basicConfig(level=logging.INFO, format="%(message)s")
-executor = futures.ThreadPoolExecutor(max_workers=max_threads)
-threadpool_container = []  # 线程池容器
-
-headers = {
-    'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
-    'Referer': 'http://www.58.com/',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-    # 'Cookie': '这里写你的Cookie',
-    'Connection': 'close'
-}
 
 
-def handler(url):
-    try:
-        max_timeout = 3  # 最大请求超时时间
-        r = requests.get(url=url, headers=headers, timeout=max_timeout)
-        content = r.content
-        if content:
-            if not r.history:
-                logging.info(
-                    "[ ] {} -> {} -> {}".format(url, r.status_code,
-                                                r.headers['Server']))
-            else:
-                lst = []
-                for code in r.history:
-                    lst.append(code.status_code)
-                lst.append(r.status_code)
-                logging.info(
-                    "[+] {} -> {} -> {}".format(url, lst,
-                                                r.headers['Server']))
-    except:
-        pass
-    finally:
-        pass
+class Src:
+    def __init__(self, max_threads=50, max_timeout=3, url_file='url.txt'):
+        self.max_threads = max_threads  # 最大并发线程数
+        self.max_timeout = max_timeout  # 最大请求超时时间
+        self.url_file = url_file  # 同级目录下存放爆破出来的域名的文件名
 
+        self.executor = futures.ThreadPoolExecutor(max_workers=max_threads)
 
-def shutdown():
-    try:
-        while True:
-            flag = True
+        self.headers = {
+            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
+            'Referer': 'http://www.58.com/',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            # 'Cookie': '这里写你的Cookie',
+            'Connection': 'close'
+        }
 
-            for t in threadpool_container:
-                flag = flag and t.done()  # 调用是否成功取消或运行完成
+    def auth(self, url):
+        try:
+            r = requests.get(url=url, headers=self.headers,
+                             timeout=self.max_timeout)
+            content = r.content
+            if content:
+                header_list = []  # ['nginx/1.10.3', 'PHP/5.6.20']
+                header_server = r.headers.get('Server')
+                header_XPoweredBy = r.headers.get('X-Powered-By')
 
-            if flag:
-                executor.shutdown()  # 清理池
+                if header_XPoweredBy is not None:
+                    header_list.append(header_XPoweredBy)
+                if header_server is not None:
+                    header_list.append(header_server)
 
-                break
-    except:
-        pass
-
-
-def run():
-    try:
-        with open('url.txt') as f:
-            while True:
-                line = f.readline()
-                if line:
-                    url = "http://" + line.strip()
-                    t = executor.submit(handler, url)
-                    threadpool_container.append(t)
-                    # threading.Thread(target=handler, args=(url,)).start()
+                if not r.history:
+                    logging.info(
+                        "[ ] {} => {} => {}".format(url, r.status_code,
+                                                    header_list))
                 else:
-                    break
-    except Exception as e:
-        # print(e)
-        pass
-    finally:
-        # logging.info('[+] 所有线程已启动，请等待域名验证完成!')
-        shutdown()
+                    status_lst = []
+                    for code in r.history:
+                        status_lst.append(code.status_code)
+                        status_lst.append(r.status_code)
+                    logging.info(
+                        "[+] {} => {} => {}".format(url, status_lst,
+                                                    header_list))
+        except:
+            pass
+
+    def dispatcher(self):
+        try:
+            with open(self.url_file) as f:
+                while True:
+                    line = f.readline()
+                    if line:
+                        url = "http://" + line.strip()  # 去掉每行末尾的\r\n
+                        self.executor.submit(self.auth, url)
+                    else:
+                        break
+        except Exception:
+            pass
+        finally:
+            logging.info('[+] 所有线程已启动，请等待域名验证完成!')
 
 
 if __name__ == '__main__':
-    start = datetime.datetime.now()
-    run()
-    end = datetime.datetime.now()
-    delta = (end - start).total_seconds()
-    logging.info("[*] 所有线程已结束！总耗时{}秒".format(delta))
+    src58 = Src(max_threads=100, max_timeout=3, url_file='url.txt')
+    src58.dispatcher()
