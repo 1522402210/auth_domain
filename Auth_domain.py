8ce525db049ce4a3ed4fd266b7390a1aaec422a3
ihoney <102505481@qq.com>
2018-02-25 00:56:13 +0800

    增加TITLE识别

--- Auth_domain.py
+++ Auth_domain.py
@@ -1,14 +1,16 @@
 # -*- coding: utf8 -*-
 import requests
 import logging
+import re
+from bs4 import BeautifulSoup
 from concurrent import futures
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
 class Src:
-    def __init__(self, max_threads=50, max_timeout=3, url_file='url.txt'):
-        self.max_threads = max_threads  # 最大并发线程数
+    def __init__(self, max_threads=25, max_timeout=3, url_file='url.txt'):
+        self.max_threads = max_threads  # 最大线程数
         self.max_timeout = max_timeout  # 最大请求超时时间
         self.url_file = url_file  # 同级目录下存放爆破出来的域名的文件名
 
@@ -16,7 +18,7 @@ class Src:
 
         self.headers = {
             'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36",
-            'Referer': 'http://www.58.com/',
+            # 'Referer': 'http://www.58.com/',
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
             # 'Cookie': '这里写你的Cookie',
             'Connection': 'close'
@@ -24,31 +26,63 @@ class Src:
 
     def auth(self, url):
         try:
-            r = requests.get(url=url, headers=self.headers,
-                             timeout=self.max_timeout)
-            content = r.content
+            r = requests.get(url=url, headers=self.headers, timeout=self.max_timeout)
+            content = r.content.decode()
+            # if content:
+            # print(content)
+            # re.S单行模式
+            # regex = re.compile('''<script(?:.*?)>(?:.*?)location.href=(?:[\'\"])(?P<redirect_url>.*?)(?:[\'\"])(?:.*?)</script>''', re.S)
+            # matcher_redirect_url = regex.search(content)
+            # print(matcher_redirect_url.group(0))
+            # if matcher_redirect_url:
+            #     url = url + '/' + str(matcher_redirect_url[0])
+            #     print(url)
+            #     r = requests.get(url=url, headers=self.headers, timeout=self.max_timeout)
+            #     content = r.content.decode()
+
             if content:
+                # logging.info(content)
+
+                # # re.S单行模式
+                # regex = re.compile('''<script(?:.*)>(?:.*)location.href=(?:[\'\"])(?P<redirect_url>.*)(?:[\'\"])(?:.*)</script>''', re.S)
+                # matcher_redirect_url = regex.findall(content)
+                # if matcher_redirect_url:
+                #     logging.info(matcher_redirect_url)
+                #     pass
+
                 header_list = []  # ['nginx/1.10.3', 'PHP/5.6.20']
+
                 header_server = r.headers.get('Server')
                 header_XPoweredBy = r.headers.get('X-Powered-By')
 
-                if header_XPoweredBy is not None:
-                    header_list.append(header_XPoweredBy)
-                if header_server is not None:
-                    header_list.append(header_server)
+                if header_XPoweredBy is not None: header_list.append(header_XPoweredBy)
+                if header_server is not None: header_list.append(header_server)
 
                 if not r.history:
-                    logging.info(
-                        "[ ] {} => {} => {}".format(url, r.status_code,
-                                                    header_list))
+
+                    title_list = []
+                    url_title = re.search(r'<title>(.*)</title>', content)
+                    # print('===', url_title)
+                    if url_title:
+                        url_title = url_title.group(1).strip().strip('\r').strip('\n')[:30]
+                        title_list.append(url_title)
+
+                    logging.info("[   ] {}   {}   {}   {}".format(url, r.status_code, header_list, title_list))
                 else:
-                    status_lst = []
+                    status_lst = []  # [301, 200, 302, 200]
+                    title_list = []
+
+                    url_title = ""
                     for code in r.history:
+                        url_title = re.search(r'<title>(.*)</title>', content)
+                        if url_title:
+                            url_title = url_title.group(1).strip().strip('\r').strip('\n')[:30]
+
                         status_lst.append(code.status_code)
-                        status_lst.append(r.status_code)
-                    logging.info(
-                        "[+] {} => {} => {}".format(url, status_lst,
-                                                    header_list))
+                    status_lst.append(r.status_code)
+                    title_list.append(url_title)
+
+                    logging.info("[+++] {}   {}   {}   {}".format(url, status_lst, header_list, title_list))
         except:
             pass
 
@@ -58,14 +92,13 @@ class Src:
                 while True:
                     line = f.readline()
                     if line:
-                        url = "http://" + line.strip()  # 去掉每行末尾的\r\n
+                        url = "http://" + str(line.strip())  # 去掉每行末尾的\r\n
+                        # self.headers['Referer'] = url
                         self.executor.submit(self.auth, url)
                     else:
                         break
         except Exception:
             pass
-        # finally:
-        #     logging.info('[+] 所有线程已启动，请等待域名验证完成!')
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# Auth_domain 子域名验证工具
-使用异步多线程并行，对所有收集到的子域名做验证(仅验证状态码、服务器类型)
+# Auth_domain 域名验证工具
+
+写这个脚本的初衷是想模仿bugscan的一个端口指纹识别功能，看图:)：
+![bugscan](bugscan.png)
+
+基本的端口扫描和端口指纹识别(www,smtp,mongodb,...)、Banner识别(Http响应中的Server,X-Powered-By,...)，这些功能都挺有用的。
+
+端口扫描部分自己也写好了，纯粹的socket实现，但还是感觉没有masscan加nmap好用
+masscan可以扫全端口：sudo masscan 11.22.33.44 -p1-65535
+nma指定开放端口识别Banner：sudo nmap -A --script=banner -p T:21,22,23,25,53,80,111,139,161,389,443 11.22.33.44
+
+接着依葫芦画瓢实现了一个可以获取域名响应码、Banner(Server、X-Powered-By)、网页TITLE的脚本:
 
 
 #### 几种子域名收集方式：
@@ -8,5 +18,35 @@
 2. https://github.com/Chora10/FuzzDomain
 3. 法师的Layer子域名挖掘机
 ```
+将整理后的子域名存放在url.txt文件中。
 
-2018年01月08日20:39:28
+#### 输出主要分两部分：
+![result](result.png)
+
+1.状态码：如果有多个验证码，代表访问该域名所经过的所有跳转后的状态码，比如 http://jingyan.ganji.com => [302, 200] 就经过了一次临时跳转才停止，使用$ curl -I http://jingyan.ganji.com -L也可以看到整个跳转过程：
+```
+$ curl -I http://jingyan.ganji.com -L
+HTTP/1.1 302 Found
+Server: Tengine
+Date: Thu, 22 Feb 2018 17:42:21 GMT
+Content-Type: text/html; charset=UTF-8
+Connection: keep-alive
+Cache-Control: private, must-revalidate
+Location: http://jingyan.ganji.com/pc/shouye
+expires: -1
+
+HTTP/1.1 200 OK
+Server: Tengine
+Date: Thu, 22 Feb 2018 17:42:21 GMT
+Content-Type: text/html; charset=UTF-8
+Connection: keep-alive
+Vary: Accept-Encoding
+Cache-Control: private, must-revalidate
+```
+
+2.Banner信息就简单了，就是获取Response中的HTTP 头中对应的值。
+
+3.网页TITLE。
+
+
+2018-02-25 00:47:27
